2dd18f1fde31f2079fedbaa0b9b134a7520e7048
nadikakpathirana <nadikakpathirana@gmail.com>
2023-02-20 21:42:48 +0530

    Updated image upload options.

--- api/routes/services.js
+++ api/routes/services.js
@@ -32,9 +32,9 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({
     storage: storage,
-    limits: {
-        fileSize: 1024 * 1024 * 5
-    },
+    // limits: {
+    //     fileSize: 1024 * 1024 * 5
+    // },
     fileFilter: fileFilter
 });
 
