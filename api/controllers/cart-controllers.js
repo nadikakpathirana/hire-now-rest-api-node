008ea0431631f7c8486ecb40ceb81d90d9cabe6f
nadikakpathirana <nadikakpathirana@gmail.com>
2023-02-22 20:16:21 +0530

    Cart functionality updated.

--- api/controllers/cart-controllers.js
+++ api/controllers/cart-controllers.js
@@ -2,10 +2,21 @@ const mongoose = require("mongoose");
 
 const Cart = require("../models/cart");
 
+function calculate_age(dob) {
+    try {
+        var diff_ms = Date.now() - dob.getTime();
+        var age_dt = new Date(diff_ms);
+        return Math.abs(age_dt.getUTCFullYear() - 1970);
+    } catch (e) {
+        return 24
+    }
+
+}
+
 // get cart list of a buyer
 exports.get_cart_items_of_a_buyer = (req, res, next) => {
     const buyerID = req.params.buyerID;
-    Cart.find({"buyerID": buyerID}).populate('service').populate('buyer')
+    Cart.find({"buyerID": buyerID}).populate('service').populate('seller')
         .exec()
         .then(docs => {
             if (docs.length > 0) {
@@ -16,19 +27,23 @@ exports.get_cart_items_of_a_buyer = (req, res, next) => {
                             _id: doc._id,
                             service: {
                                 service: {
+                                    _id: doc.service._id,
                                     title: doc.service.title,
                                     serviceImg: doc.service.serviceImg,
                                     description: doc.service.description,
                                     rateOfPayment: doc.service.rateOfPayment,
                                     price: doc.service.price,
                                     category: doc.service.category,
-                                    _id: doc.service._id,
                                     rating: 5
                                 },
                                 seller: {
-                                    _id: doc.buyer._id,
-                                    name: doc.buyer.username,
-                                    proPic: doc.buyer.proPic,
+                                    _id: doc.seller._id,
+                                    name: doc.seller.username,
+                                    proPic: doc.seller.proPic,
+                                    job: doc.seller.job,
+                                    availability: doc.seller.availability,
+                                    city: doc.seller.location,
+                                    age: calculate_age(doc.dob),
                                     rating: 6,
                                 }
                             }
@@ -51,7 +66,8 @@ exports.add_new_cart_item = (req, res, next) => {
     const cart = new Cart({
         _id: new mongoose.Types.ObjectId(),
         buyer: req.body.buyer,
-        service: req.body.service
+        service: req.body.service,
+        seller: req.body.seller,
     })
     cart
         .save()
@@ -61,7 +77,8 @@ exports.add_new_cart_item = (req, res, next) => {
                 createdCartItem: {
                     _id: result._id,
                     buyer: result.buyer,
-                    service: result.service
+                    service: result.service,
+                    seller: result.seller
                 }
             })
         })
--- api/models/cart.js
+++ api/models/cart.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const cartSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     buyer: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    service: { type: mongoose.Schema.Types.ObjectId, ref: "Service", required: true }
+    service: { type: mongoose.Schema.Types.ObjectId, ref: "Service", required: true },
+    seller: { type: mongoose.Schema.Types.ObjectId, ref: "Service", required: true },
+
 });
 
 module.exports = mongoose.model('Cart', cartSchema);
